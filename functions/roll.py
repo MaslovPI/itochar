fb8db44a30acd648d33482cebb1d53ba4232ec59
Pavel Maslov <dnfanl@gmail.com>
2026-01-03 11:58:41 +0100

    Ability scores

---
+++ functions/roll.py
@@ -0,0 +1,13 @@
+import random
+
+
+def roll(dimensions):
+    if dimensions < 1:
+        raise ValueError(f"Wrong dimensions: {dimensions}")
+    return random.randint(1, dimensions)
+
+
+def rollMultipleAccumulate(number, dimensions):
+    if number < 1:
+        raise ValueError(f"Wrong number: {dimensions}")
+    return sum(roll(dimensions) for _ in range(number))
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from functions.roll import roll, rollMultipleAccumulate
+
+
 def main():
-    print("Hello from itochar!")
+    strength = rollMultipleAccumulate(3, 6)
+    dexterity = rollMultipleAccumulate(3, 6)
+    willpower = rollMultipleAccumulate(3, 6)
+    high = max(strength, dexterity, willpower)
+
+    hit_protection = roll(6)
+    print(f"Strength: {strength}")
+    print(f"Dexterity: {dexterity}")
+    print(f"Willpower: {willpower}")
+    print(f"Highest Ability Score: {high}")
+    print(f"Hit Protection: {hit_protection}")
 
 
 if __name__ == "__main__":
